3747a0babc7581c249951418b50c3430dc898e50
Aleem Uddin <aleemuddin966@gmail.com>
2018-05-09 13:42:51 +0530

    added eslint

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,7 +1,7 @@
 import { test, Greeting } from '../src/index';
 
 describe('Basic test', () => {
-    it("should return undefined", () => {
+    it('should return undefined', () => {
         test(new Greeting())
     })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-export class Greeting{
+export class Greeting {
     greet: string
+    constructor() {
+        this.greet = null
+    }
 }
-export function test(greeting: Greeting){
+export function test(greeting: Greeting) {
     console.log(greeting.greet)
 }
+// const a = 'a'
